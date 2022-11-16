48877fff58d72ac7d5d5102c05dc8e9fa6f64532
201902483donghoLee <201902483@hufs.ac.kr>
2022-11-16 11:52:20 +0900

    firstfile mobile1012

--- App.js
+++ App.js
@@ -1,20 +1,121 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Button, View, ScrollView, StyleSheet, Text, Image, ImageBackground } from "react-native";
+import Constants from 'expo-constants';
+import { useState } from "react";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+const App = () => {
+  
+  //const flexDirection = ['row', 'row-revers', 'column', 'column-reverse']
+  const [flexDirection , setflexDirection] = useState(0);
+
+  const Square = () => {
+    const sqStyle = {
+      width : 50,
+      height : 50,
+      backgroundColor : randomcolor () 
+    };
+    return <View style = {sqStyle} /> ;
+
+
+    return <View style={sqStyle} />;
+  }
+  return (<>
+    <View style={{paddingTop:Constants.statusBarHeight}}></View>
+    <View style={[styles.container, styles.playingSpace]}>
+      <Text>글을 씁니다.</Text>
+      <Text>글을 또 씁니다.</Text>
+      
+      <Image source={require('./assets/favicon.png')}/>
+      <ImageBackground
+        style={{width:200, height:200}}
+        source={{
+          uri: 'https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg',
+        }}
+      > 
+        <Text>
+          한미 군 당국은 5일 북한의 중거리 탄도미사일(IRBM) 
+          도발에 대응해 동해상으로 연합 지대지미사일 사격을 했다고 합동참모본부가 밝혔다.
+          이번 연합 지대지미사일 사격에서 우리 군과 주한미군은 에이태큼스(ATACMS) 2발씩 
+          모두 4발을 동해상으로 발사해 가상표적을 정밀타격했다. 이는 북한의 추가 도발을
+          억제하기 위한 연합전력의 대응 능력을 보여준 것이라고 합참은 설명했다.
+        </Text>
+      
+      </ImageBackground>
     </View>
-  );
+
+    <ScrollView style={[styles.container]}>
+      <View style = {[styles.controlSpace]}>
+        <View style={[styles.buttonView]}>
+          <Button 
+          title="CHANGE FLEX DIRECTION" 
+          onPress={() => console.log('Click CHANGE FLEX EIRECTION')}
+          />
+        </View>
+        <View style={[styles.buttonView]}>
+          <Button title="CHANGE JUSTIFY CONTENT"
+          onPress={() => console.log('Click CHANGE JUSTIFY CONTENT')} 
+          />
+        </View>
+        <View style={[styles.buttonView]}>
+          <Button title="CHANGE ALIGN ITEMS"
+          onPress={() => console.log('Click CHANGE ALIGN ITEMS')} 
+          />
+        </View>
+        <View style={[styles.buttonView]}>
+          <Button title="CHANGE DIRECTION" 
+          onPress={() => console.log('Click CHANGE DIRECTION')}
+          />
+        </View>
+        <View style={[styles.buttonView]}>
+          <Button title="CHAGE FLEX WRAP" 
+          onPress={() => console.log('Click CHAGE FLEX WRAP')}
+          />
+        </View>
+        <View style={[styles.buttonView]}>
+          <Button title="ADD SQUARE"
+          onPress={() => console.log('Click ADD SQUARE')} 
+          />
+        </View>
+        <View style={[styles.buttonView]}>
+          <Button title="DELETE SQUARE" 
+          onPress={() => console.log('Click DELETE SQUARE')}
+          />
+        </View>
+      </View>
+      
+
+    </ScrollView>
+
+  </>);
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    height: '50%'
   },
+  playingSpace: {
+    backgroundColor: 'white',
+    borderColor: 'blue',
+    borderWidth: 3
+
+  },
+  controlSpace: {
+    backgroundColor: '#F5F5F5',
+    flexDirection: 'row',
+    flexWrap: 'wrap'
+  },
+
+  buttonView: {
+
+    width:'50%',
+    padding:10
+  }
 });
+
+const randomcolor = () => {
+  return '' , replace(/0/)
+}
+
+
+
+export default App;
