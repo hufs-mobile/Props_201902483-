d1ab69e55c8da9e1ca2e29dea674146c1927acc7
201902483donghoLee <201902483@hufs.ac.kr>
2022-11-16 14:44:58 +0900

    201902483firstproject1019

--- App.js
+++ App.js
@@ -4,82 +4,60 @@ import Constants from 'expo-constants';
 import { useState } from "react";
 
 const App = () => {
-  
-  //const flexDirection = ['row', 'row-revers', 'column', 'column-reverse']
-  const [flexDirection , setflexDirection] = useState(0);
+
+  const flexDirections = ['row', 'rew-reverse', 'column', 'column-reverse'];
+
+  const [flexDirection, setFlexDirection] = useState(0);
 
   const Square = () => {
     const sqStyle = {
-      width : 50,
-      height : 50,
-      backgroundColor : randomcolor () 
+      width: 50,
+      height: 50,
+      backgroundColor:randomHexColor(),
     };
-    return <View style = {sqStyle} /> ;
-
-
     return <View style={sqStyle} />;
-  }
+  };
+
+  const [squares, setSquares] = useState([Square(), Square(), Square()]);
+
+
   return (<>
     <View style={{paddingTop:Constants.statusBarHeight}}></View>
-    <View style={[styles.container, styles.playingSpace]}>
-      <Text>글을 씁니다.</Text>
-      <Text>글을 또 씁니다.</Text>
-      
-      <Image source={require('./assets/favicon.png')}/>
-      <ImageBackground
-        style={{width:200, height:200}}
-        source={{
-          uri: 'https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg',
-        }}
-      > 
-        <Text>
-          한미 군 당국은 5일 북한의 중거리 탄도미사일(IRBM) 
-          도발에 대응해 동해상으로 연합 지대지미사일 사격을 했다고 합동참모본부가 밝혔다.
-          이번 연합 지대지미사일 사격에서 우리 군과 주한미군은 에이태큼스(ATACMS) 2발씩 
-          모두 4발을 동해상으로 발사해 가상표적을 정밀타격했다. 이는 북한의 추가 도발을
-          억제하기 위한 연합전력의 대응 능력을 보여준 것이라고 합참은 설명했다.
-        </Text>
-      
-      </ImageBackground>
+    <View style={[styles.container, styles.playingSpace, hookedStyles]}>
+      { squares.map(elem => elem) }      
     </View>
 
     <ScrollView style={[styles.container]}>
       <View style = {[styles.controlSpace]}>
         <View style={[styles.buttonView]}>
-          <Button 
-          title="CHANGE FLEX DIRECTION" 
-          onPress={() => console.log('Click CHANGE FLEX EIRECTION')}
+          <Button title="CHANGE FLEX DIRECTION"               
+           onPress={() => changeSetting(flexDirection, flexDirections, setFlexDirection)}
           />
         </View>
         <View style={[styles.buttonView]}>
-          <Button title="CHANGE JUSTIFY CONTENT"
-          onPress={() => console.log('Click CHANGE JUSTIFY CONTENT')} 
+          <Button title="CHANGE JUSTIFY CONTENT" 
+            onPress={() => console.log("CHANGE JUSTIFY CONTENT")}
           />
         </View>
         <View style={[styles.buttonView]}>
-          <Button title="CHANGE ALIGN ITEMS"
-          onPress={() => console.log('Click CHANGE ALIGN ITEMS')} 
-          />
+          <Button title="CHANGE ALIGN ITEMS" 
+            onPress={() => console.log("CHANGE ALIGN ITEMS")}/>
         </View>
         <View style={[styles.buttonView]}>
           <Button title="CHANGE DIRECTION" 
-          onPress={() => console.log('Click CHANGE DIRECTION')}
-          />
+            onPress={() => console.log("CHANGE DIRECTION")}/>
         </View>
         <View style={[styles.buttonView]}>
           <Button title="CHAGE FLEX WRAP" 
-          onPress={() => console.log('Click CHAGE FLEX WRAP')}
-          />
+            onPress={() => console.log("CHAGE FLEX WRAP")}/>
         </View>
         <View style={[styles.buttonView]}>
-          <Button title="ADD SQUARE"
-          onPress={() => console.log('Click ADD SQUARE')} 
-          />
+          <Button title="ADD SQUARE" 
+            onPress={() => setSquares([...squares, Square()])}/>
         </View>
         <View style={[styles.buttonView]}>
           <Button title="DELETE SQUARE" 
-          onPress={() => console.log('Click DELETE SQUARE')}
-          />
+            onPress={() => setSquares(squares.filter((v, i)=>i !=squares.length-1))}/>
         </View>
       </View>
       
@@ -112,10 +90,10 @@ const styles = StyleSheet.create({
   }
 });
 
-const randomcolor = () => {
-  return '' , replace(/0/)
-}
-
-
+const randomHexColor = () => {
+  return '#000000'.replace(/0/g, () => {
+    return (~~(Math.random() * 16)).toString(16);
+  });
+};
 
 export default App;
